6dcb40f639b8973a63608ed6ecb9e72e6eb1a048
haris49 <hariis.459@gmail.com>
2025-12-29 15:11:36 +0700

    add update siswa API

--- app/src/main/java/com/example/questapi/apiservice/ServiceApiSiswa.kt
+++ app/src/main/java/com/example/questapi/apiservice/ServiceApiSiswa.kt
@@ -3,6 +3,7 @@ package com.example.questapi.apiservice
 import retrofit2.http.Body
 import retrofit2.http.GET
 import retrofit2.http.POST
+import retrofit2.http.PUT
 import retrofit2.http.Query
 
 interface ServiceApiSiswa {
@@ -17,5 +18,11 @@ interface ServiceApiSiswa {
 
     @POST("siswa.php")
     suspend fun postSiswa(
+        @Body data: DataSiswa
+    ): retrofit2.Response<Void>
+
+    @PUT("siswa.php")
+    suspend fun editSatuSiswa(
+        @Query("id") id: Int,
         @Body data: DataSiswa
     ): retrofit2.Response<Void>
