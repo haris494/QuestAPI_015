fd052ad4a6482058002d3942426317eaa5756828
haris49 <hariis.459@gmail.com>
2025-12-29 15:08:15 +0700

    add getSatuSiswa API

--- app/src/main/java/com/example/questapi/apiservice/ServiceApiSiswa.kt
+++ app/src/main/java/com/example/questapi/apiservice/ServiceApiSiswa.kt
@@ -1,8 +1,14 @@
 package com.example.questapi.apiservice
 
 import retrofit2.http.GET
+import retrofit2.http.Query
 
 interface ServiceApiSiswa {
 
     @GET("siswa.php")
     suspend fun getSiswa(): ResponseSiswa
+
+    @GET("siswa.php")
+    suspend fun getSatuSiswa(
+        @Query("id") id: Int
+    ): ResponseSatuSiswa
