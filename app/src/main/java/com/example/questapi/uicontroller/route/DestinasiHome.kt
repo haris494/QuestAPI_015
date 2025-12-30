fa54f9af67bd48fa3dd89c9098fd1405f754db31
haris49 <hariis.459@gmail.com>
2025-12-30 22:49:43 +0700

    menambah konfigurasi navigasi home

---
+++ app/src/main/java/com/example/questapi/uicontroller/route/DestinasiHome.kt
@@ -0,0 +1,9 @@
+package com.example.questapi.uicontroller.route
+
+import com.example.questapi.R
+
+
+object DestinasiHome : DestinasiNavigasi {
+    override val route = "home"
+    override val titleRes = R.string.app_name
+}
