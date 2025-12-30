26078dd4bc8df5a1f8bc44b8f4aa478f86831bd7
haris49 <hariis.459@gmail.com>
2025-12-30 22:53:29 +0700

    menambah viewmodel detail siswa

---
+++ app/src/main/java/com/example/questapi/viewmodel/DetailViewModel.kt
@@ -0,0 +1,55 @@
+package com.example.questapi.viewmodel
+
+import android.annotation.SuppressLint
+import androidx.compose.runtime.getValue
+import androidx.compose.runtime.mutableStateOf
+import androidx.compose.runtime.setValue
+import androidx.lifecycle.SavedStateHandle
+import androidx.lifecycle.ViewModel
+import androidx.lifecycle.viewModelScope
+import com.example.questapi.modeldata.DataSiswa
+import com.example.questapi.repositori.RepositoryDataSiswa
+import com.example.questapi.uicontroller.route.DestinasiDetail
+import kotlinx.coroutines.launch
+import retrofit2.HttpException
+import com.example.questapi.apiservice.ResponseAksi
+import java.io.IOException
+
+sealed interface StatusUIDetail {
+    data class Success(val satusiswa: DataSiswa) : StatusUIDetail
+    object Error : StatusUIDetail
+    object Loading : StatusUIDetail
+}
+class DetailViewModel (savedStateHandle: SavedStateHandle, private val repositoryDataSiswa: RepositoryDataSiswa): ViewModel() {
+    private val idSiswa: Int = checkNotNull(savedStateHandle[DestinasiDetail.itemIdArg])
+    var statusUIDetail: StatusUIDetail by mutableStateOf(StatusUIDetail.Loading)
+        private set
+
+    init {
+        getSatuSiswa()
+    }
+
+    fun getSatuSiswa() {
+        viewModelScope.launch {
+            statusUIDetail = StatusUIDetail.Loading
+            statusUIDetail = try {
+                StatusUIDetail.Success(satusiswa = repositoryDataSiswa.getSatuSiswa(idSiswa))
+            } catch (e: IOException) {
+                StatusUIDetail.Error
+            } catch (e: HttpException) {
+                StatusUIDetail.Error
+            }
+        }
+    }
+
+    @SuppressLint("SuspiciousIndentation")
+    suspend fun hapusSatuSiswa() {
+        val resp: ResponseAksi = repositoryDataSiswa.hapusSatuSiswa(idSiswa)
+
+        if (resp.success) {
+            println("Sukses Hapus Data : ${resp.message}")
+        } else {
+            println("Gagal Hapus Data : ${resp.message}")
+        }
+    }
+}
